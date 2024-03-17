988d9fecd53f25600cec1466e0a612f0bf6eccdb
moradxdev <morad.mshihdeh2k@gmail.com>
2024-03-18 01:26:16 +0200

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(express.json())
 
-app.get('/top-ten', async (req, res) => {
+app.get('/top-ten', authenticateToken, async (req, res) => {
     try {
         const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
         const data = response.data;
